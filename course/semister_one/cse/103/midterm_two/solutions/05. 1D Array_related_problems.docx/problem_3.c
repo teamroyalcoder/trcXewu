5f548da24cd1169aa859ec7c680d544ac119202e
Asaduzzaman Atik <asadatik.dev@gmail.com>
2023-03-24 06:16:36 +0600

    I always skip the hardest work in the usiverse, commit messages is one of them!

---
+++ course/semister_one/cse/103/midterm_two/solutions/05. 1D Array_related_problems.docx/problem_3.c
@@ -0,0 +1,32 @@
+/**
+ * WAP that will take n integer numbers into an array, and then print all the integers into
+ * reverse order (from the last valid index to index 0).
+ * 
+ * Solve (No issue remains) - @mrasadatik
+ * 
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int term, sum = 0;
+
+    scanf("%d", &term);
+
+    int integers[term];
+
+    for(int i = 0; i < term; i++) {
+        scanf("%d", &integers[i]);
+        if(integers[i] % 2 == 0) {
+            sum += integers[i];
+        }
+    }
+
+    printf("\n");
+
+    printf("%d ", sum);
+
+    return 0;
+
+} //main
