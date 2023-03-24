bc088153f5126a462c967bd9acbee7540a246876
Asaduzzaman Atik <asadatik.dev@gmail.com>
2023-03-24 06:10:14 +0600

    Update problem_1.c

--- course/semister_one/cse/103/midterm_two/solutions/05. 1D Array_related_problems.docx/problem_1.c
+++ course/semister_one/cse/103/midterm_two/solutions/05. 1D Array_related_problems.docx/problem_1.c
@@ -2,18 +2,6 @@
  * WAP that will take n integer numbers into an array, and then print all the integers into
  * reverse order (from the last valid index to index 0).
  * 
- * ----------------------------------------------------------------
- * Sample input:                | 5
- *                              | 1 2 3 4 5
- *                              |
- *                              |
- * ----------------------------------------------------------------
- * Sample output:               | 5 4 3 2 1
- *                              |
- *                              |
- *                              |
- * ----------------------------------------------------------------
- * 
  * Solve (No issue remains) - @mrasadatik
  * 
 */
